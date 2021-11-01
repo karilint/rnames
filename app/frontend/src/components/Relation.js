bbccae21641ba6e80c6cc7f172d403f2e267c6a7
chzesa <chzesa@gmail.com>
2021-11-01 19:46:27 +0200

    Account for possibility of name being deleted

--- app/frontend/src/components/Relation.js
+++ app/frontend/src/components/Relation.js
@@ -3,12 +3,19 @@ import { useDispatch, useSelector } from 'react-redux'
 import { loadServerData } from '../services/server'
 import { updateRel, deleteRel } from '../store/relations/actions'
 import { SnameOption } from './SnameOption'
+import { formatStructuredName } from '../utilities'
 
 export const Relation = ({ data }) => {
 	const dispatch = useDispatch()
-	const snames = useSelector(v =>
-		v.sname.concat(loadServerData('structured_names')).map(v => v.id)
-	)
+	const [snames, name1text, name2text] = useSelector(v => {
+		const sname1 = v.map[data.name1]
+		const sname2 = v.map[data.name2]
+		return [
+			v.sname.concat(loadServerData('structured_names')).map(v => v.id),
+			sname1 ? formatStructuredName(sname1, v) : '',
+			sname2 ? formatStructuredName(sname2, v) : ''
+		]
+	})
 
 	const update = ({ target }, field) => {
 		const r = { ...data }
@@ -29,7 +36,7 @@ export const Relation = ({ data }) => {
 			<input
 				name='name1'
 				type='text'
-				value={data.name1 == -1 ? '' : data.name1}
+				value={data.name1 == -1 ? '' : name1text}
 				onChange={e => update(e, 'name1')}
 				list={id1}
 			/>
@@ -43,7 +50,7 @@ export const Relation = ({ data }) => {
 			<input
 				name='name2'
 				type='text'
-				value={data.name2 == -1 ? '' : data.name2}
+				value={data.name2 == -1 ? '' : name2text}
 				onChange={e => update(e, 'name2')}
 				list={id2}
 			/>
