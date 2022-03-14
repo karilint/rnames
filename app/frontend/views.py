f721809cbf964726b1a5f78b4f714efb1dca9cfb
chzesa <chzesa@gmail.com>
2022-03-14 12:29:32 +0200

    Change wizard's url patterns

--- app/frontend/views.py
+++ app/frontend/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required, permission_required
 # Create your views here.
 @login_required
 @permission_required('rnames_app.add_reference', raise_exception=True)
-def index(request, *args, **kwargs):
+def index(request, pk=None, *args, **kwargs):
     names = list(Name.objects.filter(is_active=True).values('id', 'name'))
     locations = list(Location.objects.filter(is_active=True).values('id', 'name'))
     qualifier_names = list(QualifierName.objects.filter(is_active=True).values('id', 'name'))
@@ -13,13 +13,12 @@ def index(request, *args, **kwargs):
     references = list(Reference.objects.filter(is_active=True).values('id', 'title', 'first_author', 'link', 'year', 'doi'))
     structured_names = list(StructuredName.objects.filter(is_active=True).values('id', 'location_id', 'name_id', 'qualifier_id', 'reference_id', 'remarks'))
 
-    reference_id = request.GET.get('amendId', None)
-    amend_info = {'amend': reference_id != None}
+    amend_info = {'amend': pk != None}
 
-    if reference_id:
-        ref = Reference.objects.filter(is_active=True).get(pk=reference_id)
-        rels = list(Relation.objects.filter(is_active=True, reference_id=reference_id).values('id', 'name_one_id', 'name_two_id', 'belongs_to'))
-        amend_info['referenceId'] = reference_id
+    if pk:
+        ref = Reference.objects.filter(is_active=True).get(pk=pk)
+        rels = list(Relation.objects.filter(is_active=True, reference_id=pk).values('id', 'name_one_id', 'name_two_id', 'belongs_to'))
+        amend_info['referenceId'] = pk
         amend_info['relations'] = rels
 
     return render(request, 'frontend/index.html', {
