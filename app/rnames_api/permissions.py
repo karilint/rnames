ad49b175201ef6a52ebc5cf0babc9937816e08b4
chzesa <chzesa@gmail.com>
2022-04-04 11:38:12 +0300

    Remove custom key parser

--- app/rnames_api/permissions.py
+++ app/rnames_api/permissions.py
@@ -3,14 +3,5 @@ import json
 from rest_framework_api_key.permissions import BaseHasAPIKey
 from rnames_api.models import UserApiKey
 
-class KeyParser:
-	def get(self, request):
-		if request.content_type == 'application/json':
-			data = json.loads(request.body)
-			return data['api_key']
-
-		return None
-
 class HasUserApiKey(BaseHasAPIKey):
 	model = UserApiKey
-	key_parser = KeyParser()
