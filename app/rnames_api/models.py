4039c468fc88c6ce5b8b5434360d9473b8ac31a1
chzesa <chzesa@gmail.com>
2022-04-04 13:46:34 +0300

    Implement api key use logging for entry creation

--- app/rnames_api/models.py
+++ app/rnames_api/models.py
@@ -34,5 +34,8 @@ class KeyRelation(ApiKeyUsed):
 class KeyStratigraphicQualifier(ApiKeyUsed):
 	stratigraphic_qualifier = models.ForeignKey(rnames_models.StratigraphicQualifier, on_delete=models.CASCADE)
 
+class KeyTimeSlice(ApiKeyUsed):
+	time_slice = models.ForeignKey(rnames_models.TimeSlice, on_delete=models.CASCADE)
+
 class KeyStructuredName(ApiKeyUsed):
 	structured_name = models.ForeignKey(rnames_models.StructuredName, on_delete=models.CASCADE)
