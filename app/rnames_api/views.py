1a9c75ac10978500244525014ee436efdffe7f91
chzesa <chzesa@gmail.com>
2022-06-03 14:53:41 +0300

    Remove inline-relations api endpoint

--- app/rnames_api/views.py
+++ app/rnames_api/views.py
@@ -120,12 +120,3 @@ class TimeSliceViewSet(ApiViewSet):
 class BinningViewSet(viewsets.ReadOnlyModelViewSet):
 	queryset = models.Binning.objects.all()
 	serializer_class = serializers.BinningSerializer
-
-class InlineRelationViewSet(viewsets.ReadOnlyModelViewSet):
-	queryset = (models.Relation.objects.is_active()
-			.prefetch_related('name_one', 'name_two', 'reference')
-			.prefetch_related('name_one__name', 'name_one__location','name_one__reference','name_one__qualifier')
-			.prefetch_related('name_two__name', 'name_two__location','name_two__reference','name_two__qualifier')
-			.prefetch_related('name_one__qualifier__qualifier_name', 'name_one__qualifier__stratigraphic_qualifier')
-			.prefetch_related('name_two__qualifier__qualifier_name', 'name_two__qualifier__stratigraphic_qualifier'))
-	serializer_class = serializers.RelationInlineSerializer
