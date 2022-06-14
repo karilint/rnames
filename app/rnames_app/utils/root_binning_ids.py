eced1301ff8dc842489885e929e0584c848eba88
bjoekroe <bjorn.kroger@helsinki.fi>
2022-06-14 14:37:53 +0300

    updated binning
    # now root-binning is a file that collects input to binning, runs binning and gives three output binning results:
    1) a binning based on structured name ids
    2) a simplified binning with structured names
    3) a binning with age constraints
    There are several options for binning with different binning schemes, binning algorithms and age constraints

--- app/rnames_app/utils/root_binning_ids.py
+++ app/rnames_app/utils/root_binning_ids.py
@@ -15,10 +15,12 @@ import binning_fun_PBDB # to be changedx
 
 ###################
 #download relations from RNames API
-# this takes c. 0.5h
+# this takes up to c. 0.5h
 # any possibility to speed this up?
 start = time.time()
 url = "https://rnames-staging.it.helsinki.fi/api/relations/?format=json&inline=true&page_size=10000"
+# if url does not work use:
+#url = "https://rnames-staging.it.helsinki.fi/api/relations/?format=json&inline=true"    
 print(url)
 response = requests.get(url)
 response_json = response.json()
@@ -37,19 +39,29 @@ while True:
 	flat_json = pd.json_normalize(response_json['results'], sep='_')
 	res_rels_RN_raw = pd.concat([res_rels_RN_raw, pd.DataFrame(flat_json)])
     
-#print(res_rels_RN)
 ende = time.time()
 
 print("download took:",(ende - start)/60, "mins")
 
-
-
-res_rels_RN = res_rels_RN_raw[['id', 'name_one_id', 'name_two_id','name_one_qualifier_stratigraphic_qualifier_name', 
-                           'name_two_qualifier_stratigraphic_qualifier_name', 'reference_id','reference_year',
-                               'reference_title']]
 #print(res_rels_RN_raw.keys())
 
-
+# make relations two sided
+res_rels_RN = res_rels_RN_raw[['id', 'name_one_id', 'name_two_id', 
+                               'name_one_name_name', 'name_two_name_name',
+                               'name_one_qualifier_qualifier_name_name',
+                               'name_two_qualifier_qualifier_name_name',                              
+                               'reference_id','reference_year','reference_title']]
+res_rels_RN_rw = res_rels_RN_raw[['id', 'name_two_id', 'name_one_id',
+                               'name_two_name_name', 'name_one_name_name',
+                               'name_one_qualifier_qualifier_name_name', 
+                               'name_two_qualifier_qualifier_name_name',                                
+                               'reference_id','reference_year','reference_title']]
+res_rels_RN_rw.columns = ['id', 'name_one_id', 'name_two_id', 
+                               'name_one_name_name', 'name_two_name_name',
+                               'name_one_qualifier_qualifier_name_name',
+                               'name_two_qualifier_qualifier_name_name',                              
+                               'reference_id','reference_year','reference_title']
+res_rels_RN_tw = pd.concat([res_rels_RN, res_rels_RN_rw], axis=0)
 
 ###################
 #download structured names from RNames API
@@ -57,7 +69,9 @@ res_rels_RN = res_rels_RN_raw[['id', 'name_one_id', 'name_two_id','name_one_qual
 # any possibility to speed this up?
 
 start = time.time()
-url = "https://rnames-staging.it.helsinki.fi/api/structured-names/?inline=true&format=json&page_size=10000"
+#url = "https://rnames-staging.it.helsinki.fi/api/structured-names/?inline=true&format=json&page_size=10000"
+# if url does not work use instead
+url = "https://rnames-staging.it.helsinki.fi/api/structured-names/?inline=true&format=json"
 print(url)
 response = requests.get(url)
 response_json = response.json()
@@ -76,19 +90,17 @@ while True:
 	flat_json = pd.json_normalize(response_json['results'], sep='_')
 	res_sn_raw = pd.concat([res_sn_raw, pd.DataFrame(flat_json)])
     
-#print(res_rels_RN)
 ende = time.time()
 
 print("download took:",(ende - start)/60, "mins")
 
+#print(res_sn_raw.keys())
 res_sn = res_sn_raw[['id', 'name_name', 'qualifier_qualifier_name_name','location_name', 
                            'reference_first_author', 'reference_year','reference_id']]
-#print(res_sn_raw.keys())
 
 ###################
 # object with all structured names with name 'not specified'
 # this is also needed for binning
-
 not_spec = res_sn[res_sn['name_name']== 'not specified']
 not_spec = not_spec[['id']]
 #print(not_spec)
@@ -98,16 +110,18 @@ not_spec = not_spec[['id']]
 ## read time scales
 # this should be via API as soon as functional
 # see my email
-
 ts_names = pd.read_csv ('ts_names.csv')
 t_scales = pd.read_csv ('time_scales.csv')
 
 ###################
 # define time scale scheme
-# this should be via input form on frontend
+# this should be via input form on frontend,
+# before implementing it into frontent can you test if the script runs
+# with these three
 # examples: 'Ordovician time bins (Webby et al., 2004)'
 # 'Periods (ICS, 2020)'
-binning_scheme = 'Stages (ICS, 2020)' # this is input
+# 'Stages (ICS, 2020)'
+binning_scheme = 'Ordovician time bins (Webby et al., 2004)'# this is input
 
 
 ###################
@@ -118,7 +132,7 @@ binning_algorithm = 'combined' # this is input
 
 ###################
 ###################
-## binning of structured names imported from PBDB
+## binning of structured names imported from PBDB without reference
 PBDB_names_binned= binning_fun_PBDB.bin_fun_PBDB(c_rels = res_rels_RN_raw, c_strat= res_sn_raw,
                            binning_scheme = binning_scheme, ts_names = ts_names, t_scales = t_scales)
 
@@ -155,12 +169,24 @@ binned_raw.rename(columns={'youngest':'youngest_id', 'name_name': 'youngest_name
 binned_raw = binned_raw[['name_id', 'name', 'qualifier_name','oldest_id', 'oldest_name',
                            'youngest_id', 'youngest_name', 'refs']]
 
+###################
+###################
+# output 1:
+# all structural names with distinct distinct id
+# binned to binning scheme
+binned_raw = binned_raw.drop_duplicates()
+
+###################
+###################
+
 #print(resi_binned)
 
-# produce generalised binning
-# unique names
-uni_binned = binned_raw[['name', 'qualifier_name']]
+# generate generalised binning
+# unique names without absolute time
+uni_binned = binned_raw[~binned_raw['qualifier_name']=='mya']
+uni_binned = uni_binned[['name', 'qualifier_name']]
 uni_binned = uni_binned.drop_duplicates()
+
 
 # get time bins
 x_names = ts_names[ts_names['ts_name']==binning_scheme]
@@ -197,5 +223,52 @@ binned_generalised.rename(columns={'name_name': 'oldest'},inplace = True)
 binned_generalised = binned_generalised[['name', 'oldest', 'youngest_id']]
 binned_generalised = pd.merge(binned_generalised, res_sn, left_on="youngest_id", right_on="id")
 binned_generalised.rename(columns={'name_name': 'youngest'},inplace = True)
+
+###################
+###################
+# output 2:
+# all structural names with identical name and qualifier name
+# binned to binning scheme
+# we could call this "simplified binning"
 binned_generalised = binned_generalised[['name', 'oldest', 'youngest']]
 #print(binned_generalised) 
+
+## adding absolute ages to the binnings
+# Two options are available:
+# + adding the age constraints used in the PBDB: 'PBDB_ages'
+# + adding the age constraints of own RNames entries: 'RN_ages'
+# define age constraints
+agecon = 'RN_ages'
+
+xbinned = pd.concat([binned_raw['youngest_id'], binned_raw['oldest_id']], ignore_index=True).drop_duplicates()
+xage_res_rels = res_rels_RN_tw[(res_rels_RN_tw['name_one_id'].isin(xbinned))  &
+                 (res_rels_RN_tw['name_two_qualifier_qualifier_name_name']=='mya')]
+
+def agebinbin (xage_res_rels,agecon):
+    if agecon== 'PBDB_ages':
+        xage_res_rels_x = xage_res_rels.loc[(xage_res_rels["reference_title"]=="Paleobiology Database")]
+    if agecon== 'RN_ages':
+        xage_res_rels_x = xage_res_rels.loc[~(xage_res_rels["reference_title"]=="Paleobiology Database")]   
+    if (xage_res_rels_x.shape[0]==0):
+        print('No absolute time scale exists for binning approach.')
+    if (xage_res_rels_x.shape[0]>0):
+            xage_res_rels_x = xage_res_rels_x[['name_one_id','name_two_name_name','reference_id']]
+            agerange = xage_res_rels_x['name_one_id'].drop_duplicates()
+            binned_ages = pd.DataFrame([] * 4, columns=['name_id','oldest_age', 'youngest_age', 'ref_age'])
+            for i in range(0,len(agerange)):
+                xage_x = xage_res_rels_x[xage_res_rels_x['name_one_id'] == agerange.iloc[i]]
+                oldest = max(pd.to_numeric(xage_x['name_two_name_name']))
+                youngest = min(pd.to_numeric(xage_x['name_two_name_name']))
+                combi = pd.DataFrame([{'name_id' : xage_x['name_one_id'].iloc[0],
+                                        'oldest_age' : oldest, 'youngest_age' : youngest,
+                                        'ref_age' : xage_x['reference_id'].iloc[0]}])
+                binned_ages = pd.concat([binned_ages, combi], axis=0)
+    
+            binned_ages_o = pd.merge(binned_raw, binned_ages, left_on="oldest_id", right_on="name_id")
+            binned_ages_o = binned_ages_o.drop(['youngest_age','ref_age'], axis=1)
+            binned_ages = pd.merge(binned_ages_o, binned_ages, left_on="youngest_id", right_on="name_id")
+            binned_ages = binned_ages.drop(['oldest_age_y', 'name_id_y','name_id'], axis=1)
+            binned_ages.rename(columns={'oldest_age_x':'oldest_age', 'name_id_x': 'name_id'},inplace = True)
+            return (binned_ages)
+        
+binned_with_abs_ages = agebinbin(xage_res_rels = xage_res_rels,agecon = agecon)    
