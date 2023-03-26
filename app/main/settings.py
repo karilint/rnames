fdb997128012686f3c377b21c6251e2ed491af92
chzesa <chzesa+git@gmail.com>
2023-03-27 02:21:45 +0300

    Configure celery result backend and celery beat to eventually clean up results

--- app/main/settings.py
+++ app/main/settings.py
@@ -258,5 +258,6 @@ SELECT2_CACHE_BACKEND = 'select2'
 
 # celery settings
 CELERY_BROKER_URL = 'redis://redis:6379'
+CELERY_RESULT_BACKEND = 'redis://redis:6379'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
