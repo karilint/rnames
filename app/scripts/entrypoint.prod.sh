b2ec6ab6f1343a2ff7e44cfec1410b5db6eec52a
Jonne Sotala <jonne.sotala@gmail.com>
2025-05-15 16:36:21 +0300

    Add debugpy and restructure docker processes
    - Add debugpy package and run both celery and django with it for
    debugging
    - Separate Celery and Django processes to separate docker containers

--- app/scripts/entrypoint.prod.sh
+++ app/scripts/entrypoint.prod.sh
@@ -5,6 +5,4 @@ set -e
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 
-celery -A main beat --detach -s /var/run/celerybeat-schedule
-celery -A main worker --detach -l INFO
 gunicorn main.wsgi:application --bind 0.0.0.0:8000
